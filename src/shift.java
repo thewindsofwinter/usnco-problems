e71c217b24fd07eab109238861a7b5b4f8d20bad
Andy Tang <winterwind2022@gmail.com>
2021-01-06 22:14:18 -0600

    fixed problems for 2006 and added few comments

--- src/shift.java
+++ src/shift.java
@@ -14,15 +14,17 @@ public class shift {
     public static void main(String[] args) throws IOException, FileNotFoundException {
         
         // Fix issues I've created
-        int year = 2003;
-        int start = 53;
+        int year = 2006;
+        int start = 58;
         int end = 59;
         
+        // If you need to shift backwards, use this block
         /* for(int i = start; i <= end; i++) {
             BufferedImage in = ImageIO.read(new File("tests/" + year + "/" + i + ".png"));
-            ImageIO.write(in, "png", new File("tests/" + year + "/" + (i - 1) + ".png"));
+            ImageIO.write(in, "png", new File("tests/" + year + "/" + (i - 2) + ".png"));
         } */
         
+        // If you need to shift forward, use this block
         for(int i = end; i >= start; i--) {
             BufferedImage in = ImageIO.read(new File("tests/" + year + "/" + i + ".png"));
             ImageIO.write(in, "png", new File("tests/" + year + "/" + (i + 1) + ".png"));
