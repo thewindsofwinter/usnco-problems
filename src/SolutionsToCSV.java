269fc6802f7768ee6faf113ffb64382d2517b60b
Andy Tang <winterwind2022@gmail.com>
2021-01-29 13:16:33 -0600

    initial rendering of images

--- src/SolutionsToCSV.java
+++ src/SolutionsToCSV.java
@@ -1,3 +1,12 @@
+
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+import org.apache.pdfbox.pdmodel.PDDocument;
+import org.apache.pdfbox.rendering.ImageType;
+import org.apache.pdfbox.rendering.PDFRenderer;
+import org.apache.pdfbox.tools.imageio.ImageIOUtil;
+
 /*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
@@ -13,8 +22,29 @@ public class SolutionsToCSV {
     /**
      * @param args the command line arguments
      */
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         // TODO code application logic here
+        File dirpath = new File("C:/Users/winterwind/Downloads/Exam PDFs");
+        File[] exams = dirpath.listFiles();
+        
+        // Practice with one at first [WIP]
+        for(int i = 0; i < exams.length; i++) {
+            PDDocument pd = PDDocument.load(exams[i]);
+            
+            // Get the year of the exam for folder naming
+            String year = exams[i].getName().substring(0, 4);
+            PDFRenderer pdfRenderer = new PDFRenderer(pd);
+            
+            // Create directory 
+            new File("answers/" + year).mkdirs();
+            int pageNumber = pd.getNumberOfPages() - 1;
+            
+            BufferedImage currentPage = pdfRenderer.renderImageWithDPI(pageNumber, 300, ImageType.RGB);
+            ImageIOUtil.writeImage(currentPage, "answers/" + year + "/Exam-" + (pageNumber+1) + ".png", 300);
+            
+            // Make sure to close at end
+            pd.close();
+        }
     }
     
 }
