ee0be266a63000c611c41b02b6198f6c797071e7
Andy Tang <winterwind2022@gmail.com>
2021-02-09 11:05:54 -0600

    make program

---
+++ src/ExtensionManager.java
@@ -0,0 +1,42 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+import java.io.*;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.Iterator;
+import java.util.stream.Stream;
+
+/**
+ *
+ * @author winterwind
+ */
+public class ExtensionManager {
+    
+    // Lots of code, including this function, from stackoverflow
+    public static File changeExtension(File f, String newExtension) {
+        int i = f.getName().lastIndexOf('.');
+        String name = f.getName().substring(0,i);
+        return new File(f.getParent(), name + newExtension);
+    }
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) throws IOException {
+        // TODO code application logic here
+        File directoryPath = new File("./tests");
+        
+        Stream<Path> str = Files.walk(Paths.get("./tests"));
+        Iterator<Path> it = str.iterator();
+        
+        while(it.hasNext()) {
+            System.out.println(it.next());
+        }
+
+    }
+}
